bbfe8158bc9f4d78bf411eec688925becda047a7
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-07-22 17:54:05 +0200

    Created knexfile config

---
+++ backend/knexfile.ts
@@ -0,0 +1,24 @@
+import type { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: Knex.Config = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  migrations: {
+    directory: './src/migrations',
+    extension: 'ts'
+  },
+  seeds: {
+    directory: './src/seeds',
+    extension: 'ts'
+  }
+}
+
+export default config;
