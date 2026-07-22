786bf48c90a9a35a9a2760d3603cbf21737f35ff
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-07-22 22:08:52 +0200

    Created first iteration of login endpoint

--- backend/src/routes/user/Login.ts
+++ backend/src/routes/user/Login.ts
@@ -2,14 +2,19 @@ import { Request, Response, NextFunction } from 'express';
 import { UsersDatabase } from '../../database/Database';
 
 
-const userLogin = (req: Request, res: Response, next: NextFunction) => {
+const userLogin = async (req: Request, res: Response, next: NextFunction) => {
   let db:UsersDatabase = new UsersDatabase();
 
   console.log("Tried to login. Data: " + req.body.username + ";    " + req.body.password);
 
-  db.CreateUser(req.body.username, req.body.password);
+  let result:number = await db.CheckForUser(req.body.username, req.body.password);
 
-  res.status(200).send("OK");
+  if (result != -1) {
+    res.status(200).json({ id: result });
+  }
+  else {
+    res.status(401).json({ cause: "Wrong credentials" });
+  }
 };
 
 export default userLogin;
