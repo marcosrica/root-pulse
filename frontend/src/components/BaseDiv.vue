cbe9a060a76491785778ae2d9224463b5a1976c5
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-07-23 17:44:52 +0200

    Added div component with no liquid glass

---
+++ frontend/src/components/BaseDiv.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="card">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+    .card {
+        padding: 10px;
+        
+        border: 3px solid var(--div-border);
+
+        border-radius: 20px;
+    }
+</style>
