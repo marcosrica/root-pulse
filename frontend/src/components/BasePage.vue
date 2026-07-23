f6a994ed4a1602b3554333f6ef2d61fb38e6de5e
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-07-23 15:08:38 +0200

    Started menu separation on the base page

--- frontend/src/components/BasePage.vue
+++ frontend/src/components/BasePage.vue
@@ -3,9 +3,65 @@
 </script>
 
 <template>
-
+    <div class="background">
+        <div class="sideMenuWrapper">
+            <div class="sideMenu">
+                
+            </div>
+        </div>
+        
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .background {
+        width: 100dvw;
+        height: 100dvh;
 
+        background-color: var(--background);
+
+        display: flex;
+        flex-direction: row;
+
+        @media(orientation: portrait) {
+            flex-direction: column-reverse;
+        }
+    }
+
+    .sideMenuWrapper {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        
+        width: 4%;
+        height: 100%;
+
+        @media(orientation: portrait) {
+            width: 100%;
+            height: 8%;
+        }
+    }
+
+    .sideMenu {
+        width: 80%;
+        height: 90%;
+
+        border: 2px solid var(--div-border);
+        border-radius: 20px;
+
+        @media(orientation: portrait) {
+            width: 100%;
+            height: 80%;
+        }
+    }
+
+    .content {
+        background-color: orange;
+
+        flex: 1;
+    }
 </style>
