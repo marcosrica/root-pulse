b9204cff0aca316e6638ac17b27075692bb0c438
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-07-24 19:14:26 +0200

    Added a floating panel

--- frontend/src/components/FloatingPanel.vue
+++ frontend/src/components/FloatingPanel.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+const props = defineProps <{
+  show: boolean
+  hide: () => void
+}>();
 
 </script>
 
 <template>
-  <div class="background active">
-      
-  </div>
+    <div :class="['background', show ? 'active' : 'inactive']" v-on:click="hide">
+        <div :class="['panel', show ? '' : 'panelInactive']" @click.stop>
+            <slot></slot>
+        </div>
+    </div>
 </template>
 
 <style scoped>
@@ -18,19 +24,41 @@
     height: 100dvh;
 
     z-index: 1000000;
-    transition: 0.5s;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    transition: 0.6s;
 }
 
 .active {
     background-color: #000000b5;
+    pointer-events: all;
 }
 
 .inactive {
-    background-color: transparent;;
+    background-color: transparent;
+    pointer-events: none;
+}
+
+.panel {
+    background-color: var(--background);
+    border: 3px solid var(--div-border);
+    border-radius: 20px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    transition: 0.4s;
+    pointer-events: all;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.panelInactive {
+    pointer-events: none;
+
+    top: 150%;
 }
 </style>
