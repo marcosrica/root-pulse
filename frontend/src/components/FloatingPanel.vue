3b21c83683dd4d79a5624dafceaa7aa00d29375a
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-07-24 18:22:30 +0200

    Added icons for sensors

---
+++ frontend/src/components/FloatingPanel.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="background active">
+      
+  </div>
+</template>
+
+<style scoped>
+.background {
+    position: fixed;
+    top: 0px;
+    right: 0px;
+    
+    width: 100dvw;
+    height: 100dvh;
+
+    z-index: 1000000;
+    transition: 0.5s;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.active {
+    background-color: #000000b5;
+}
+
+.inactive {
+    background-color: transparent;;
+}
+</style>
