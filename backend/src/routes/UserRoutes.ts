1f8e536c23b68cfd147d1e73e954263acdda5356
Robert Voong <rvoong1408@gmail.com>
2026-07-25 20:38:39 +0200

    Test de peticiones a endpoints de usuarios

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+//usando el archivo index.ts podemos importar la logica de los endpoints en una linea, para no hacer muchas lineas en un futuro
 import { userLogin, createNewAccount } from './user';
 
 const UserRoutes = Router();
