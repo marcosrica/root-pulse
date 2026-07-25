b8c32779fba1591e743d65a9da82a6b163af16d4
Robert Voong <rvoong1408@gmail.com>
2026-07-25 23:04:29 +0200

    before try out

--- backend/src/routes/user/CreateAccount.ts
+++ backend/src/routes/user/CreateAccount.ts
@@ -5,22 +5,36 @@ import { userDbresultStatus } from '../../errors/UserDBstatus';
 const createNewAccount = async (req: Request, res: Response, next: NextFunction) => {
   let db: UsersDatabase = new UsersDatabase();
 
-  let username: string = req.body.username;
-  let password: string = req.body.password;
+  // let username: string = req.body.username;
+  // let password: string = req.body.password;
+  //Esto es preferencia personal, y si fueran mas atributos de body podriamos crear un type para los tipos pero solo son dos
+  const { username, password }: { username: string, password: string } = req.body;
 
-  if (username != "" && password != null) {
-    let result:userDbresultStatus = await db.CreateUser(username, password);
+  //primero comprobaciones de error y luego añadimos el usuario + codigos de estado correctos
+  if(!username || !password){
+    return res.status(400).json({ cause: "Must fill all fields"});
+  }
+  //
+  try {
+    
+  } catch (error) {
+    
+  }
 
-    if (result == userDbresultStatus.AllOK) {
-      res.status(200).json({ result: "OK" });
-    }
-    else {
-      res.status(401).json({ cause: "Forbidden" });
-    }
-  }
-  else {
-    res.status(500).json({ cause: "Empty fields" });
-  }
+  //
+  // if (username != "" && password != null) {
+  //   let result:userDbresultStatus = await db.CreateUser(username, password);
+
+  //   if (result == userDbresultStatus.AllOK) {
+  //     res.status(200).json({ result: "OK" });
+  //   }
+  //   else {
+  //     res.status(401).json({ cause: "Forbidden" });
+  //   }
+  // }
+  // else {
+  //   res.status(500).json({ cause: "Empty fields" });
+  // }
 };
 
 export default createNewAccount;
