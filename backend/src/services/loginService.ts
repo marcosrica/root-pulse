b76c36168eef0020c93a14e17a4e916a84b82f2c
Robert Voong <rvoong1408@gmail.com>
2026-07-26 23:57:10 +0200

    loginService implemented

--- backend/src/services/loginService.ts
+++ backend/src/services/loginService.ts
@@ -1,8 +1,42 @@
 import { UsersDatabase } from '../database/Database';
 import bcrypt from 'bcrypt';
+import { generateToken } from '../utils/token'
 
-const login = async() => {
-    return"";
+type loginResponse = {
+    id: number,
+    username: string,
+    token: string
+}
+
+type loginData = {
+    username: string,
+    password: string
+}
+export class login {
+    constructor(private db: UsersDatabase) {}
+
+    async execute(data: loginData): Promise<loginResponse> {
+        //first check that all fields are filled before consulting db
+        if(!data.username) throw new Error('username field must be filled');
+        if(!data.password) throw new Error('password field must be filled');
+
+        //comprobamos que exista el usuario con el username
+        const existingU = await this.db.findByUsername(data.username.toLocaleLowerCase());
+        if(!existingU) throw new Error('There is no username with this username');
+
+        //checking if the password is the same as in the database, this method compares a plain text password with the 
+        //hashed password in our database and returns a boolean
+        const isValid = await bcrypt.compare(data.password, existingU.password);
+        if(!isValid) throw new Error('Invalid password ');
+
+        //if everything is alright we can generate the token
+        const token = await generateToken({ id: existingU.id,
+                                            username: existingU.username
+                                        });
+    
+        return { id: existingU.id, username: existingU.username, token: token };
+
+    }
 }
 
 export default login;
