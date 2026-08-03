6c77fd8fd0cf127ffb5d04c496ea7c0702013be2
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-08-03 15:57:11 +0200

    Started work on  the Sensor page itself

---
+++ frontend/src/views/Sensor.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+    import BasePage from '@/components/BasePage.vue';
+    import BaseDiv from '@/components/BaseDiv.vue';
+    import { useI18n } from '@/locales/i18n';
+    import type { SensorInfo } from '@/Utilities/types/SensorInfo';
+    
+    //Library for easy translation features
+    const { t } = useI18n();
+    
+    let data: SensorInfo = {
+        name: "Sensor1",
+        alias: "",
+        lastMeasure: 61,
+        minAlert: 30,
+        lastConnection: new Date('December 17, 2021 04:28:00')
+    };
+</script>
+
+<template>
+    <BasePage location="home">
+        <BaseDiv class="headerDiv">
+            <h1 class="marginless headerText"> {{data.alias != "" ? data.alias : data.name}} </h1>
+        </BaseDiv>
+
+        <BaseDiv class="partDiv">
+            <div class="leftDiv">
+                <h1 class="marginless lastMeasureCuantity"> {{data.lastMeasure}}% </h1>
+                <p class="marginless"> {{t("sensor.lastMeasure")}} </p>
+            </div>
+
+            <div class="leftDiv">
+                
+            </div>
+
+            <div class="leftDiv">
+                
+            </div>
+        </BaseDiv>
+    </BasePage>
+</template>
+
+<style scoped>
+    .marginless {
+        margin: 0px;
+    }
+
+    .headerText {
+        font-size: 40px;
+        text-align: center;
+    }
+
+    .headerDiv {
+        margin-bottom: 30px;
+    }
+
+    .partDiv {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .leftDiv {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        gap: 0px;
+    }
+
+    .lastMeasureCuantity {
+        font-size: 60px;
+    }
+</style>
