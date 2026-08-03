97b13b33841dfa8f9071239490636c32cfda2f56
Robert Voong <rvoong1408@gmail.com>
2026-08-03 23:15:16 +0200

    Throw new errors instead of a general Error class

--- backend/src/services/createAccountService.ts
+++ backend/src/services/createAccountService.ts
@@ -1,6 +1,7 @@
 import { UsersDatabase } from '../database/Database';
 import bcrypt from 'bcrypt';
 import { generateToken } from '../utils/token';
+import { ValidationError, ExistenceError, DatabaseError, TokenError } from '../utils/errors';
 //Tipo de respuesta que devuelve un usuario credo, con fecha y todo
 export type UserRegistered = {
     id: number;
@@ -15,26 +16,26 @@ export class register {
 
  async execute( username: string, password: string): Promise<UserRegistered> {
     //Comprobacion de campos con informacion
-    if(!username.trim()) throw new Error('Username field must not be empty');
-    if(!password.trim()) throw new Error('Password field must not be empty');
-    if(password.length < 6) throw new Error('Password must be at least of 6 caracters');
-    if(password.toLowerCase() == password) throw new Error('Passowrd must have at least one capital letter');
-    if(password.includes(" ") || password.trim() != password) throw new Error('Password must not contain spaces');
+    if(!username.trim()) throw new ValidationError('NO_USERNAME', 'The username field must not be empty');
+    if(!password.trim()) throw new ValidationError('NO_PASSWORD', 'Password field must not be empty');
+    if(password.length < 6) throw new ValidationError('PASSWORD_SHORT', 'Password must be at least of 6 caracters');
+    if(password.toLowerCase() == password) throw new ValidationError('REQUIRED_CAPITAL_LETTER', 'Passowrd must have at least one capital letter');
+    if(password.includes(" ") || password.trim() != password) throw new ValidationError('NO_SPACES', 'Password must not contain spaces');
 
     //comprobacion de usuario existente en minusculas para evitar situaciones de User1 = Maria; User2 = MaRiA
     const existingU = await this.db.findByUsername(username.toLowerCase());
-    if(existingU) throw new Error('This username is already on use');
+    if(existingU) throw new ExistenceError('USERNAME_ON_USE', 'This username is already on use');
 
     //Una vez que comprobamos que se puede insertar el usuario hasheamos la contraseña
     const hashedPassword = await bcrypt.hash(password, 10);
 
     //Ahora guardamos al nuevo usuario username en minusculas, en la base de datos y nos quedamos con el Id con el que se crea
     const userId = await this.db.createUser( username.toLowerCase(), hashedPassword);
-    if(!userId) throw new Error('Internal db error');
+    if(!userId) throw new DatabaseError('DB_ERROR','Error tryng to create user');
 
     //Si el usuario se ha creado correctamente le generamos un token con el regitro para que entre directamente
     const token = generateToken({id: userId, username: username});
-    if(!token) throw new Error('Error al crear el token')
+    if(!token) throw new TokenError('TOKEN_ERROR','Error while generating token')
 
     //si el usuario se ha creado devolvemos los datos mas el token
     return {
--- backend/src/services/loginService.ts
+++ backend/src/services/loginService.ts
@@ -1,6 +1,7 @@
 import { UsersDatabase } from '../database/Database';
 import bcrypt from 'bcrypt';
 import { generateToken } from '../utils/token'
+import { ValidationError, ExistenceError, DatabaseError, TokenError } from '../utils/errors';
 
 type loginResponse = {
     id: number,
@@ -17,17 +18,17 @@ export class login {
 
     async execute(data: loginData): Promise<loginResponse> {
         //first check that all fields are filled before consulting db
-        if(!data.username) throw new Error('username field must be filled');
-        if(!data.password) throw new Error('password field must be filled');
+        if(!data.username) throw new ValidationError('NO_USERNAME','username field must be filled');
+        if(!data.password) throw new ValidationError('NO_PASSWORD','password field must be filled');
 
         //comprobamos que exista el usuario con el username
         const existingU = await this.db.findByUsername(data.username.toLowerCase());
-        if(!existingU) throw new Error('There is no username with this username');
+        if(!existingU) throw new ExistenceError('NO_USERNAME_FOUND','There is no username with this username');
 
         //checking if the password is the same as in the database, this method compares a plain text password with the 
         //hashed password in our database and returns a boolean
         const isValid = await bcrypt.compare(data.password, existingU.password);
-        if(!isValid) throw new Error('Invalid password');
+        if(!isValid) throw new ValidationError('INVALID_PASSWORD', 'Invalid password');
 
         //if everything is alright we can generate the token
         const token = generateToken({ id: existingU.id,
