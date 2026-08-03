ddcdf85da72694d1a242e3039311907a48130500
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-08-03 16:57:14 +0200

    New sensor page, now very well min value alert

--- frontend/src/views/Sensor.vue
+++ frontend/src/views/Sensor.vue
@@ -4,22 +4,27 @@
     import { useI18n } from '@/locales/i18n';
     import type { SensorInfo } from '@/Utilities/types/SensorInfo';
     import { onMounted, ref } from 'vue';
+    import PageInput from '@/components/PageInput.vue';
     
     //Library for easy translation features
     const { t } = useI18n();
 
     const valueOk = ref<boolean>(true);
+    const newMin = ref<string>("0");
     
     let data: SensorInfo = {
         name: "Sensor1",
         alias: "",
         lastMeasure: 55,
         minAlert: 30,
-        lastConnection: new Date('December 17, 2021 04:28:00')
+        lastConnection: new Date('December 17, 2020 04:28:00')
     };
 
     onMounted(() => {
         valueOk.value = data.lastMeasure > data.minAlert;
+        newMin.value = data.minAlert;
+        
+        let vl = Date.now() - data.lastConnection.getDate();
     })
 </script>
 
@@ -39,8 +44,10 @@
                 
             </div>
 
-            <div class="leftDiv">
-                
+            <div class="leftDiv minAlertDiv">
+                    <div class="minAlertIcon"></div>
+                    <input type="number" class="minAlertInput" v-model="newMin" />
+                    <h1 class="marginless headerText"> % </h1>
             </div>
         </BaseDiv>
     </BasePage>
@@ -72,6 +79,7 @@
         justify-content: center;
         align-items: center;
         text-align: center;
+        width: auto;
         gap: 0px;
     }
 
@@ -85,5 +93,32 @@
 
     .Ok {
         color: var(--ok)
+    }
+
+    .minAlertDiv {
+        display: flex;
+        flex-direction: row;
+        width: auto;
+        min-width: 200px;
+    }
+
+    .minAlertIcon {
+        min-height: 50px;
+        height: 100%;
+        aspect-ratio: 1;
+        background-color: var(--alert);
+        mask-image: url('/icons/Alert.svg');
+        mask-size: contain;
+    }
+
+    .minAlertInput {
+        background-color: transparent;
+        width:80px;
+        height: 80%;
+        border: 1px solid white;
+        border-radius: 20px;
+        color: white;
+        font-size: 40px;
+        display: flex;
     }
 </style>
