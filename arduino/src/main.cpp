e2128520f0f8641e1c7ed0e9d9ecfb17c7ca6141
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-08-27 09:21:08 +0200

    Added code for making GET petition from arduino

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,11 +1,9 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 #include "secrets.h"
 
-
-void setup() {
-  Serial.begin(115200);
-
+void connectToWifi() {
   Serial.println("Connecting to WIFI...");
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -20,6 +18,36 @@ void setup() {
 
   Serial.print("IP adress: ");
   Serial.println(WiFi.localIP());
+}
+
+void makeGetRequest(char url[]) {
+  HTTPClient http;
+  String serverUrl = SERVER_IP + url;
+  http.begin(serverUrl);
+
+  int httpCode = http.GET();
+
+  if(httpCode > 0) {
+    Serial.printf("HTTP GET code: %d \n", httpCode);
+
+    //If the response is OK, read the payload
+    if(httpCode == 200) {
+      String payload = http.getString();
+      Serial.println("Response payload:");
+      Serial.println(payload);
+    }
+  }
+  else {
+    Serial.printf("GET request failed, error: %s \n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  connectToWifi();
 }
 
 void loop() {
