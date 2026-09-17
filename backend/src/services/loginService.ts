45d428c4aea866f01f8a157a7cf1572b73015938
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-09-17 21:43:35 +0200

    Fixed login service indentantion and restructured it

--- backend/src/services/loginService.ts
+++ backend/src/services/loginService.ts
@@ -4,40 +4,40 @@ import { generateToken } from '../utils/token'
 import { ValidationError, ExistenceError, DatabaseError, TokenError } from '../utils/errors';
 
 type loginResponse = {
-    id: number,
-    username: string,
-    token: string
+  id: number,
+  username: string,
+  token: string
 }
 
 type loginData = {
-    username: string,
-    password: string
+  username: string,
+  password: string
 }
 export class login {
-    constructor(private db: UsersDatabase) {}
+  constructor(private db: UsersDatabase) {}
 
-    async execute(data: loginData): Promise<loginResponse> {
-        //first check that all fields are filled before consulting db
-        if(!data.username) throw new ValidationError('NO_USERNAME','username field must be filled');
-        if(!data.password) throw new ValidationError('NO_PASSWORD','password field must be filled');
+  async execute(data: loginData): Promise<loginResponse> {
+    //First check that all the fields have been filled before consulting db
+    if(!data.username) throw new ValidationError('NO_USERNAME','username field must be filled');
+    if(!data.password) throw new ValidationError('NO_PASSWORD','password field must be filled');
 
-        //comprobamos que exista el usuario con el username
-        const existingU = await this.db.findByUsername(data.username.toLowerCase());
-        if(!existingU) throw new ExistenceError('NO_USERNAME_FOUND','There is no username with this username');
+    //Then check that an account exists with that username
+    const existingU = await this.db.findByUsername(data.username.toLowerCase());
+    if(!existingU) throw new ExistenceError('NO_USERNAME_FOUND','There is no account with this username');
 
-        //checking if the password is the same as in the database, this method compares a plain text password with the 
-        //hashed password in our database and returns a boolean
-        const isValid = await bcrypt.compare(data.password, existingU.password);
-        if(!isValid) throw new ValidationError('INVALID_PASSWORD', 'Invalid password');
+    //Checking if the provided password matches the one stored in the database
+    //Using bcrypt to compare a plain text password against a hashed password
+    const isValid = await bcrypt.compare(data.password, existingU.password);
+    if(!isValid) throw new ValidationError('INVALID_PASSWORD', 'Invalid password');
 
-        //if everything is alright we can generate the token
-        const token = generateToken({ id: existingU.id,
-                                            username: existingU.username
-                                        });
-    
-        return { id: existingU.id, username: existingU.username, token: token };
+    //If everything went on fine, we can generate the token
+    const token = generateToken({ id: existingU.id,
+      username: existingU.username
+    });
 
-    }
+    //Returning the data
+    return { id: existingU.id, username: existingU.username, token: token };
+  }
 }
 
 export default login;
