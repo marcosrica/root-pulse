e6153e0ae8675aabf4b54ae118f9166a5a5d093d
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-09-17 21:37:38 +0200

    Added infraestructure to respond to /api/auth/sensor POST queries

--- backend/src/ controllers/authControllers/index.ts
+++ backend/src/ controllers/authControllers/index.ts
@@ -1,2 +1,3 @@
 export { default as userLogin } from './Login';
 export { default as createNewAccount } from './CreateAccount';
+export { default as sensorLogin } from './SensorAuth';
--- backend/src/routes/AuthRoutes.ts
+++ backend/src/routes/AuthRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 //usando el archivo index.ts podemos importar la logica de los endpoints en una linea, para no hacer muchas lineas en un futuro
-import { userLogin, createNewAccount } from '../ controllers/authControllers';
+import { userLogin, createNewAccount, sensorLogin } from '../ controllers/authControllers';
 
 const AuthRoutes = Router();
 
 AuthRoutes.post('/login', userLogin);
 AuthRoutes.post('/newAccount', createNewAccount);
+AuthRoutes.post('/sensor', sensorLogin);
 
 export default AuthRoutes;
