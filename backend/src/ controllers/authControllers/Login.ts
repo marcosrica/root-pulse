d2dd3c23afed703c475d7639b94b12a6caa34923
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-09-17 21:38:35 +0200

    Fixed indentation

--- backend/src/ controllers/authControllers/Login.ts
+++ backend/src/ controllers/authControllers/Login.ts
@@ -11,16 +11,16 @@ const userLogin = async (req: Request, res: Response, next: NextFunction) => {
     //we try to execute the service of loging an user and if anything went bad we can catch it
     const tryLogin = await loginService.execute({username: username, password: password});
 
-res.cookie('token', tryLogin.token, {
-              httpOnly: true,  //restricts the access to the token via JavaScript
-             //secure: true --> token only send through https, as we are working in http localhost it doesnt send 
-              // sameSite: 'strict', //cookie wont be went if the petition is from an external web
-              maxAge: 3 * 60 * 60 * 1000 //cookie duration (3 hours)
-});
+    res.cookie('token', tryLogin.token, {
+      httpOnly: true,  //restricts the access to the token via JavaScript
+      //secure: true --> token only send through https, as we are working in http localhost it doesnt send 
+      // sameSite: 'strict', //cookie wont be went if the petition is from an external web
+      maxAge: 3 * 60 * 60 * 1000 //cookie duration (3 hours)
+    });
 
     return res.status(200).json({ response: "user logged in correctly",
-                                  id: tryLogin.id,
-                                  username: tryLogin.username,
+      id: tryLogin.id,
+      username: tryLogin.username,
     });
 
   } catch (error) {
